c934a38122024eb86f567249ea491aa18a4f6f9f
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2023-08-04 19:27:55 +0300

    Add routing link for post component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from "./pages/register/register.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {SinglePostComponent} from "./components/single-post/single-post.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', component: HomeComponent},
+  { path: 'post/:id', component: SinglePostComponent},
   // {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
 ];
